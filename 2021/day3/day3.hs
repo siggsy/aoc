41905237a46e6e5c7ec395f6f05e097b00f60e75
siggsy <ziga@leskovec.xyz>
2021-12-03 10:10:52 +0100

    Cleanup Day 3

--- 2021/day3/day3.hs
+++ 2021/day3/day3.hs
@@ -12,20 +12,19 @@ run input =
 parseInput :: String -> Input
 parseInput raw = map f $ lines raw
     where
-        f = map (\ y -> ord y - ord '0')
+        f = map (\ y -> ord y - ord '0' /= 0)
 
-type Input = [[Int]]
+type Input = [[Bool]]
 type Output = Int
-type Output2 = Int
 
 solve1 :: Input -> Output
 solve1 input = gamma * epsilon
     where
         gamma       = fromBinary mostCommon
         epsilon     = fromBinary $ complement mostCommon
-        mostCommon  = map (> div inLength 2) (foldl f (head input) (drop 1 input))
+        mostCommon  = map (> div inLength 2) (foldl f [0, 0 ..] input)
         inLength    = length input
-        f acc x     = zipWith (+) x acc
+        f acc x     = zipWith (\x' -> if x' then (+1) else (+0)) x acc
 
 fromBinary :: [Bool] -> Int
 fromBinary = foldl (\x y -> (if y then 1 else 0) + 2 * x) 0
@@ -38,16 +37,14 @@ solve2 input = o2 * co2
     where
         bitCriteriaO2 length'       = (>= (div length' 2 + mod length' 2))
         bitCriteriaCO2 length'      = not . bitCriteriaO2 length'
-        countOnes list              = foldl (zipWith (+)) (head list) (drop 1 list)
-        o2                          = fromBinary $ map (/= 0) $ applyCriteria bitCriteriaO2 input
-        co2                         = fromBinary $ map (/= 0) $ applyCriteria bitCriteriaCO2 input
+        countOnes                   = foldl sumZip [0, 0 ..]
+        sumZip acc x                = zipWith (\x' -> if x' then (+1) else (+0)) x acc
+        o2                          = fromBinary $ applyCriteria bitCriteriaO2 input
+        co2                         = fromBinary $ applyCriteria bitCriteriaCO2 input
         applyCriteria criteria bins = head $ foldl (f criteria) bins [0..(length $ head bins)]
-        f criteria acc x        =
+        f criteria acc x            =
             case acc of
                 [a] -> acc
                 _   -> filter (\x' -> x'!!x == condition) acc
                     where
-                        condition =
-                            if criteria (length acc) (countOnes acc !! x)
-                                then 1
-                                else 0
+                        condition = criteria (length acc) (countOnes acc !! x)
