6dc47927c35409b25aa77c1e127e0bb6acfb040c
siggsy <ziga@leskovec.xyz>
2021-12-04 08:56:37 +0100

    Remove hacky example input scraper

--- create_day.sh
+++ create_day.sh
@@ -14,20 +14,12 @@ mkdir -p $full_path
 cp template.hs $full_path/$day_name.hs
 
 # Find inputs
-echo -e "> Downoading input files"
-echo -e ">> Example input"
-example_test=$(curl --silent 'https://adventofcode.com/'$year'/day/'$day'' -H 'coockie: session='$AOC_COOKIE'' | htmlq "pre code")
-START=$(echo "$example_test" | grep -n -m1 "<code" | cut -d ':' -f1)
-END=$(echo "$example_test" | grep -n -m1 "</code" | cut -d ':' -f1)
-echo "$example_test" | sed -n -e "$START,$END p" | htmlq --text | head -n -1 > $full_path/$day_name.test.txt
-
-echo -e ">> Actual input"
+echo -e "> Downoading input file"
 curl --silent 'https://adventofcode.com/'$year'/day/'$day'/input' -H 'cookie: session='$AOC_COOKIE'' > $full_path/$day_name.txt
 
 echo -e "> Creating aliases: test, run, publish1, publish2"
 echo -e "
-alias test=\"ghc $day_name.hs && ./$day_name < $day_name.test.txt\"
-alias run=\"ghc $day_name.hs && ./$day_name < $day_name.txt\"
+alias run=\"ghc $day_name.hs && ./$day_name\"
 alias publish1=\"../../publish.sh 1\"
 alias publish2=\"../../publish.sh 2\"
 " > $full_path/aliases
