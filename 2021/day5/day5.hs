0d0b114fe3768c093aa6610515d89ee0b15464a0
siggsy <ziga@leskovec.xyz>
2021-12-05 21:11:40 +0100

    Day 5 2021 Set implementation

--- 2021/day5/day5.hs
+++ 2021/day5/day5.hs
@@ -29,9 +29,9 @@ parseInput = Prelude.map (
 type Input = [((Int, Int), (Int, Int))]
 type Output = Int
 
--- Implementation with Sets appears to be faster than with Map
-drawPaths :: [((Int, Int), (Int, Int))] -> Set (Int, Int)
-drawPaths p = snd (Prelude.foldl (\(paths, intersects) ((x1, y1), (x2, y2)) ->
+-- Implementation with Set appears to be faster than with Map
+getIntersections :: [((Int, Int), (Int, Int))] -> Set (Int, Int)
+getIntersections p = snd (Prelude.foldl (\(paths, intersects) ((x1, y1), (x2, y2)) ->
     let
         diffx       = x2 - x1
         diffy       = y2 - y1
@@ -39,7 +39,7 @@ drawPaths p = snd (Prelude.foldl (\(paths, intersects) ((x1, y1), (x2, y2)) ->
         dy          = if diffy /= 0 then div diffy (abs diffy) else 0
         xCoor'      = [x1, (x1 + dx) .. x2]
         yCoor'      = [y1, (y1 + dy) .. y2]
-        (xCoor, yCoor)  -- Create ordered pairs for faster Map creation (log(n) * n vs n)
+        (xCoor, yCoor)
             | dx == -1 = (reverse xCoor', reverse yCoor')
             | dy == -1 && dx == 0 = (xCoor', reverse yCoor')
             | otherwise = (xCoor', yCoor')
@@ -48,40 +48,15 @@ drawPaths p = snd (Prelude.foldl (\(paths, intersects) ((x1, y1), (x2, y2)) ->
             | x1 == x2  = fromAscList (zip [x1, x1 ..] yCoor)
             | y1 == y2  = fromAscList (zip xCoor [y1, y1 ..])
             | otherwise = fromAscList (zip xCoor yCoor)
-        paths' = paths `union` line
-        intersects' = (paths `intersection` line) `union` intersects 
+        paths' = paths `union` line                                     -- Store lines to check for intersections
+        intersects' = (paths `intersection` line) `union` intersects    -- Store intersections without duplicates
     in (paths', intersects')) (empty, empty) p)
-
--- Returns a map of coordinates and theri respective intersecions
--- eg. [((0,1), 3)] means 3 intersections in (0, 1)
-
--- We use transform lists to map to reduce
--- time complexity from O(n^2) to O(n * log(n))
--- drawPaths :: [((Int, Int), (Int, Int))] -> Map (Int, Int) Int
--- drawPaths = foldl (\paths ((x1, y1), (x2, y2)) ->
---     let
---         diffx       = x2 - x1
---         diffy       = y2 - y1
---         dx          = if diffx /= 0 then div diffx (abs diffx) else 0
---         dy          = if diffy /= 0 then div diffy (abs diffy) else 0
---         xCoor'      = [x1, (x1 + dx) .. x2]
---         yCoor'      = [y1, (y1 + dy) .. y2]
---         (xCoor, yCoor)  -- Create ordered pairs for faster Map creation (log(n) * n vs n)
---             | dx == -1 = (reverse xCoor', reverse yCoor')
---             | dy == -1 && dx == 0 = (xCoor', reverse yCoor') 
---             | otherwise = (xCoor', yCoor')
-
---         line
---             | x1 == x2  = fromAscList (zip (zip [x1, x1 ..] yCoor) [1,1..])
---             | y1 == y2  = fromAscList (zip (zip xCoor [y1, y1 ..]) [1,1..])
---             | otherwise = fromAscList (zip (zip xCoor yCoor) [1,1..])
---     in unionWith (+) paths line) empty
 
 solve1 :: Input -> Output
 solve1 = length
-    . drawPaths
-    . Prelude.filter (\((x1, y1), (x2, y2)) -> x1 == x2 || y1 == y2 )
+    . getIntersections
+    . Prelude.filter (\((x1, y1), (x2, y2)) -> x1 == x2 || y1 == y2 ) -- Only check for horizontal and vertical lines
 
 solve2 :: Input -> Output
 solve2 = length
-    . drawPaths
+    . getIntersections
