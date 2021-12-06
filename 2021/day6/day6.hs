68f1ad8ab0aa85dc3dc3746e3a26dfbea1ab1f17
siggsy <ziga@leskovec.xyz>
2021-12-06 08:42:51 +0100

    Day 6 2021

---
+++ 2021/day6/day6.hs
@@ -0,0 +1,51 @@
+import Data.List.Split
+import Data.List
+
+main = interact run
+
+run :: String -> String
+run input =
+    show p1 ++ "\n" ++ show p2 ++ "\n"
+    where
+        parsedInput = parseInput input
+        p1 = solve1 parsedInput
+        p2 = solve2 parsedInput
+
+parseInput :: String -> Input
+parseInput raw =
+    sortBy (\(a,_) (b,_) -> compare b a) (unionBy (\x y -> fst x == fst y)
+        (map (\x -> (head x, length x))
+            . group
+            . reverse
+            . sort
+            . Prelude.map read
+            . splitOn "," $ raw)
+        [ (i, 0) | i <- [0..8] ])
+
+
+type Input = [(Int, Int)]
+type Output = Int
+
+-- Fish are represented as (day, count)
+simulateDay :: [(Int, Int)] -> [(Int, Int)]
+simulateDay fish = 
+    (8, newCount) 
+        : (day7, count7) 
+        : (6, newCount + count6) 
+        : drop 2 (reverse (drop 1 (reverse fish')))
+    where
+        [(day7, count7), (day6, count6)]    = take 2 fish'
+        (day, newCount)                     = last fish'
+        fish'                               = map (\(day, count) -> (day - 1, count)) fish
+    
+runSimulation :: Int -> [(Int, Int)] -> [(Int, Int)]
+runSimulation days fish = foldl (\acc x -> simulateDay acc) fish [0..(days-1)]
+
+fishCount :: [(Int, Int)] -> Int
+fishCount = foldl (\nums x -> nums + snd x) 0
+
+solve1 :: Input -> Output
+solve1 = fishCount . runSimulation 80
+
+solve2 :: Input -> Output
+solve2 = fishCount . runSimulation 256
